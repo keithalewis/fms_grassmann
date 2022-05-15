f4a3da1206fa96a4a955c5f5255dbe3897b66e96
Keith A. Lewis <kal@kalx.net>
2022-05-15 06:20:48 -0400

    working

--- fms_grassman.t.cpp
+++ fms_grassman.t.cpp
@@ -8,63 +8,93 @@ using E = fms::grassmann::element<>;
 
 int test_bits()
 {
-	{
-		unsigned a(0b1), b(0b10);
-		assert(0 == perm(0b1u, a));
-		assert(1 == perm(a, b));
-		assert(-1 == sign(a, b));
-		assert(0 == perm(b, a));
-		assert(1 == sign(b, a));
-	}
 
 	return 0;
 }
+
 int main()
 {
-	test_bits();
+	E::test_bits();
 	{
+		// empty
 		E A;
-		assert(1 == A.size());
-		assert(A.contains(0));
-		assert(0 == A[0]);
-		A.trim();
 		assert(0 == A.size());
 	}
 	{
+		// number 2
 		E A(2.);
 		assert(1 == A.size());
 		assert(A.contains(0));
 		assert(2 == A[0]);
 		A.trim();
 		assert(1 == A.size());
+		A[0u] = 0;
+		A.trim();
+		assert(0 == A.size());
+	}
+	// unit extensor of i
+	auto P = [](unsigned i) { return E(1u << i); };
+
+	{
+		auto P3 = P(3);
+		assert(1 == P3.size());
+		auto p3 = *P3.begin();
+		assert(0b1000u == p3.first);
+		assert(1 == p3.second);
+		P3[8] = 0;
+		P3.trim();
+		assert(0 == P3.size());
 	}
 	{
-		E A{ {0b1u,0} };
+		auto P01 = (P(0) | P(1));
+		assert(1 == P01.size());
+		auto p01 = *P01.begin();
+		assert(0b11u == p01.first);
+		assert(-1 == p01.second);
+
+		P01 = P(1) | P(0);
+		assert(1 == P01.size());
+		auto p10 = *P01.begin();
+		assert(0b11u == p10.first);
+		assert(1 == p10.second);
+	}
+	{
+		E A(0*P(0));
 		assert(1 == A.size());
+		E A0 = 0. * P(0);
+		assert(A0 == A);
 		A.trim();
 		assert(0 == A.size());
 	}
 	{
-		E A{ {0b1u, 2} };
-		E B{ {0b10u, 3} };
-		std::cout << (A + B) << std::endl;
-		std::cout << (A - B) << std::endl;
-		std::cout << (A | B) << std::endl;
-		//std::cout << (A & A) << std::endl;
-		//std::cout << (~A) << std::endl;	
+		E A{ 2 * P(0) };
+		E B{ 3 * P(1) };
+		assert((A + B) == 2 * P(0) + 3 * P(1));
+		assert((B + A) == 2 * P(0) + 3 * P(1));
+		assert((A - B) == 2 * P(0) - 3 * P(1));
+		assert((A - B) == 2 * P(0) + -3 * P(1));
+		assert((A | B) == -6 * E(1u + 2u));
+		assert((B | A) == 6 * E(2u + 1u));
+		assert((A & A) == 2 * A);
+		assert(~A == E(~1, -2));
 	}
-	/*
-	std::map<I, A, decltype(fms::grassmann::less<I>)>	{
-		E P1{ { 0b1, 1.} };
-		E P2{ { 0b10, 1.} };
-		E P3{ { 0b100, 1.} };
+	{
+		E P1 = P(0);
+		E P2 = P(1);
+		E P3 = P(2);
 		auto P12 = P1 | P2;
 		auto P23 = P2 | P3;
 		assert(P2 == (P12 & P23));
 		auto P = P1 | P2 | P3 | (P1 + P2 + P3);
 		assert(0 == P.size());
 	}
-	*/
+	{
+		auto P1 = P(0);
+		auto P2 = P(1);
+		auto Q = 2 * P1 + 3 * P2;
+		assert(2 == (Q | P2) / (P1 | P2));
+		assert(3 == (P1 | Q) / (P1 | P2));
+	}
 
 	return 0;
 }
--- fms_grassmann.h
+++ fms_grassmann.h
@@ -12,165 +12,101 @@
 
 namespace fms::grassmann {
 
-	// graded sort
-	template<std::unsigned_integral I>
-	inline constexpr auto less = [](const I i, const I j) noexcept
-	{
-		auto cmp = std::popcount(i) - std::popcount(j);
-
-		return cmp < 0 ? true : cmp == 0 ? i < j : false;
-	};
-
-	// total number of permutations or order i,j
-	template<std::unsigned_integral I>
-	inline constexpr int perm(const I i, I j) noexcept
-	{
-		int s = 0;
-
-		for (auto k = std::bit_floor(j); 0 != std::popcount(k); k = std::bit_floor(j)) {
-			s += std::popcount(i & (k - 1));
-			j &= ~k;
-		}
-
-		return s;
-	}
-	// sign of permutation
-	template<std::unsigned_integral I>
-	inline constexpr int sign(const I i, I j) noexcept
-	{
-		return perm(i, j) & 1 ? -1 : 1;
-	}
-
-#if 0
-	//
-	// blade
-	//
-
-	// weighted single extensor
-	template<std::unsigned_integral I, typename A>
-		requires std::is_arithmetic_v<A>
-	using blade = std::pair<I, A>;
-
-	// unary minus
-	template<std::unsigned_integral I, typename A>
-		requires std::is_arithmetic_v<A>
-	inline constexpr blade<I, A> operator-(blade<I, A> a) noexcept
-	{
-		a.second = -a.second;
-
-		return a;
-	}
-
-	// scalar multiplication
-	template<std::unsigned_integral I, typename A, typename B>
-		requires std::is_arithmetic_v<A> && std::is_arithmetic_v<B>
-	inline constexpr blade<I, A>& operator*=(blade<I, A>& a, const B b) noexcept
-	{
-		a.second *= b;
-
-		return a;
-	}
-	template<std::unsigned_integral I, typename A, typename B>
-		requires std::is_arithmetic_v<A>&& std::is_arithmetic_v<B>
-	inline constexpr blade<I, A> operator*(blade<I, A> a, const B b) noexcept
-	{
-		return a *= b;
-	}
-	template<std::unsigned_integral I, typename A, typename B>
-		requires std::is_arithmetic_v<A> && std::is_arithmetic_v<B>
-	inline constexpr blade<I, A> operator*(const B b, blade<I, A> a) noexcept
-	{
-		return a *= b;
-	}
-
-	// scalar right division
-	template<std::unsigned_integral I, typename A, typename B>
-		requires std::is_arithmetic_v<A> && std::is_arithmetic_v<B>
-	inline constexpr blade<I, A>& operator/=(blade<I, A>& a, const B b) noexcept
-	{
-		a.second /= b;
-
-		return a;
-	}
-	template<std::unsigned_integral I, typename A, typename B>
-		requires std::is_arithmetic_v<A>&& std::is_arithmetic_v<B>
-	inline constexpr blade<I, A> operator/(blade<I, A> a, const B b) noexcept
-	{
-		return a /= b;
-	}
-
-	// division of compatible extensors
-	template<std::unsigned_integral I, typename A>
-		requires std::is_arithmetic_v<A>
-	inline constexpr A operator/(const blade<I, A>& a, const blade<I, A>& b) noexcept
-	{
-		return a.first == b.first ? a.second / b.second : std::numeric_limits<A>::quiet_NaN();
-	}
-
-	// progressive product
-	template<std::unsigned_integral I, typename A>
-		requires std::is_arithmetic_v<A>
-	inline constexpr blade<I, A>& operator|(blade<I, A>& a, const blade<I, A>& b) noexcept
-	{
-		if (a.first & b.first) {
-			a = blade<I, A>{};
-		}
-		else {
-			a.first |= b.first;
-			a.second = sign(a.first, b.first) * a.second * b.second;
-		}
-
-		return a;
-	}
-
-	// regressive product 
-	template<std::unsigned_integral I, typename A>
-		requires std::is_arithmetic_v<A>
-	inline constexpr blade<I, A>& operator&(blade<I, A>& a, const blade<I, A>& b) noexcept
-	{
-		if (a.first & b.first) {
-			a.first &= b.first;
-			a.second *= b.second;
-		}
-		else {
-			a = blade<I, A>{};
-		}
-	
-		return a;
-	}
-#endif // 0
-	//
 	// element of Grassmann algebra
-	// 
 	template<std::unsigned_integral I = unsigned, typename A = double>
 		requires std::is_arithmetic_v<A>
 	class element
 	{
-		using map = std::map<I, A, decltype(fms::grassmann::less<I>)>;
+		// total number of permutations to order i,j
+		static inline constexpr int perm(const I i, I j) noexcept
+		{
+			int s = 0;
+
+			// check if popcount > size/2???
+
+			for (auto k = std::bit_floor(j); 0 != std::popcount(k); k = std::bit_floor(j)) {
+				s += std::popcount(i & (k - 1));
+				j &= ~k;
+			}
+
+			return s;
+		}
+		// sign of permutation
+		static inline constexpr int sign(const I i, I j) noexcept
+		{
+			return perm(i, j) & 1 ? -1 : 1;
+		}
+
+#ifdef _DEBUG
+	public:
+		static int test_bits()
+		{
+			{
+				I a(0b1), b(0b10);
+				assert(0 == perm(0b1u, a));
+				assert(1 == perm(a, b));
+				assert(-1 == sign(a, b));
+				assert(0 == perm(b, a));
+				assert(1 == sign(b, a));
+			}
+
+			return 0;
+		}
+	private:
+#endif // _DEBUG
+
+		struct cmp {
+			constexpr bool operator()(const I i, const I j) const noexcept
+			{
+				auto cmp = std::popcount(i) - std::popcount(j);
+
+				return cmp < 0 ? true : cmp == 0 ? i < j : false;
+			}
+		};
+
+		using map = std::map<I, A, cmp>;
+		map e;
+
+		using T = map::value_type;
+		static T join(const T& a, const T& b)
+		{
+			return (a.first & b.first) ? T(0u,0) : T(a.first | b.first, sign(a.first, b.first) * a.second* b.second);
+		}
+		static T meet(const T& a, const T& b)
+		{
+			return (a.first & b.first) ? T(a.first & b.first, a.second * b.second) : T(0u, 0);
+		}
+
+
+	public:	
 		using key_type = typename map::key_type;
 		using value_type = typename map::value_type;
+		using mapped_type = typename map::mapped_type;
+		using reference = typename map::reference;
 		using iterator = typename map::iterator;
 		using const_iterator = typename map::const_iterator;
-		map e;
-	public:
-		// real number b
-		template<typename B>
-			requires std::is_arithmetic_v<B>
-		element(const B b = 0)
-			: e({ {0u, b} })
-		{ }
-		element(const value_type& b)
-			: e({ b })
-		{ }
+
 		element() = default;
 		element(const element&) = default;
 		element& operator=(const element&) = default;
+		element(element&&) = default;
+		element& operator=(element&&) = default;
 		~element() = default;
 
-		auto operator<=>(const element& f) const
-		{
-			return e <=> f.e;
-		}
+		constexpr element(I i, A a = 1)
+			: e({ std::pair(i, a) })
+		{ }
+		// real number b
+		template<typename B>
+			requires std::is_arithmetic_v<B>
+		constexpr element(const B b)
+			: element(0u, b)
+		{ }
+		constexpr explicit element(const map& e)
+			: e(e)
+		{ }
+
+		auto operator<=>(const element& f) const = default;
 
 		auto size() const
 		{
@@ -180,7 +116,11 @@ namespace fms::grassmann {
 		{
 			return e.contains(k);
 		}
-		auto operator[](const key_type& k)
+		auto operator[](const key_type& k) const
+		{
+			return e[k];
+		}
+		auto& operator[](const key_type& k)
 		{
 			return e[k];
 		}
@@ -213,6 +153,18 @@ namespace fms::grassmann {
 		element& operator-()
 		{
 			std::for_each(e.begin(), e.end(), [](auto& b) { b.second = -b.second; });
+
+			return *this;
+		}
+
+		// dagger
+		element& operator~()
+		{
+			map _e;
+			for (const auto& a : e) {
+				_e[~a.first] = sign(a.first, ~a.first) * a.second;
+			}
+			e = _e;
 
 			return *this;
 		}
@@ -283,50 +235,66 @@ namespace fms::grassmann {
 			return *this;
 		}
 
-		// progressive product
-		element& operator|=(const value_type& b)
+		// quotient of compatible extensors
+		A operator/(const value_type& b) const
 		{
-			std::erase_if(e, [&b](const auto& a) { return a.first & b.first; });
+			A q = 0;
 
-			map e_;
-			for (const auto& a : e) {
-				e_.insert({ a.first | b.first, sign(a.first, b.first) * a.second * b.second });
+			for (const auto& a : *this) {
+				if (a.first == b.first) {
+					q += a.second / b.second;
+				}
+				else {
+					q = std::numeric_limits<A>::quiet_NaN();
+					break;
+				}
 			}
-			//e = e_; !!! fails !!!
-			//e.swap(e_);
-	
-			return *this;
+
+			return q;
 		}
+		A operator/(const element& f) const
+		{
+			if (1 != f.size()) {
+				return std::numeric_limits<A>::quiet_NaN();
+			}
+
+			return operator/(*f.begin());
+		}
+
+		// progressive product
 		element& operator|=(const element& f)
 		{
-			for (const auto& b : f) {
-				operator|=(b);
-			}
-
-			return *this;
-		}
-
-		// regressive product
-		element& operator&=(const value_type& b)
-		{
-			std::erase_if(e, [&b](const auto& a) { return !(a.first & b.first); });
-
 			map e_;
 			for (const auto& a : e) {
-				e_.insert({ a.first & b.first, a.second * b.second });
+				for (const auto& b : f) {
+					auto ab = join(a, b);
+					if (ab.first and ab.second) {
+						e_[ab.first] += ab.second;
+					}
+				}
 			}
 			e = e_;
 
 			return *this;
 		}
+
+		// regressive product
 		element& operator&=(const element& f)
 		{
-			for (const auto& b : f) {
-				operator&=(b);
+			map e_;
+			for (const auto& a : e) {
+				for (const auto& b : f) {
+					auto ab = meet(a, b);
+					if (ab.first and ab.second) {
+						e_[ab.first] += ab.second;
+					}
+				}
 			}
+			e = e_;
 
 			return *this;
 		}
+
 	};
 
 }
@@ -336,7 +304,7 @@ template<std::unsigned_integral I, typename A>
 inline std::ostream& operator<<(std::ostream& os, const fms::grassmann::element<I, A>& e)
 {
 	for (const auto& [i, a] : e) {
-		os << std::showpos << a << ":" << std::hex << i << " ";
+		os << std::showpos << a << "*" << std::hex << i << " ";
 	}
 
 	return os;
@@ -355,12 +323,6 @@ inline fms::grassmann::element<I, A> operator-(fms::grassmann::element<I, A> e, 
 	return e -= f;
 }
 
-template<std::unsigned_integral I, typename A>
-	requires std::is_arithmetic_v<A>
-inline fms::grassmann::element<I, A> operator*(fms::grassmann::element<I, A> e, const fms::grassmann::element<I, A>& f)
-{
-	return e *= f;
-}
 template<std::unsigned_integral I, typename A, typename B>
 	requires std::is_arithmetic_v<A> && std::is_arithmetic_v<B>
 inline fms::grassmann::element<I, A> operator*(fms::grassmann::element<I, A> e, const B b)
@@ -368,16 +330,23 @@ inline fms::grassmann::element<I, A> operator*(fms::grassmann::element<I, A> e, 
 	return e *= b;
 }
 template<std::unsigned_integral I, typename A, typename B>
-	requires std::is_arithmetic_v<A>&& std::is_arithmetic_v<B>
+	requires std::is_arithmetic_v<A> && std::is_arithmetic_v<B>
 inline fms::grassmann::element<I, A> operator*(const B b, fms::grassmann::element<I, A> e)
 {
 	return e *= b;
 }
+
 template<std::unsigned_integral I, typename A, typename B>
-	requires std::is_arithmetic_v<A>&& std::is_arithmetic_v<B>
+	requires std::is_arithmetic_v<A> && std::is_arithmetic_v<B>
 inline fms::grassmann::element<I, A> operator/(fms::grassmann::element<I, A> e, const B b)
 {
 	return e *= 1/b;
+}
+template<std::unsigned_integral I, typename A>
+	requires std::is_arithmetic_v<A>
+inline A operator/(const fms::grassmann::element<I, A>& e, const fms::grassmann::element<I, A>& f)
+{
+	return e / f;
 }
 
 template<std::unsigned_integral I, typename A>
@@ -393,17 +362,11 @@ inline fms::grassmann::element<I, A> operator&(fms::grassmann::element<I, A> e, 
 {
 	return e &= f;
 }
-/*
+
 // dagger
 template<std::unsigned_integral I, typename A>
-inline fms::grassmann::element<N, A> operator~(const fms::grassmann::element<N, A>& e)
+inline fms::grassmann::element<I, A> operator~(fms::grassmann::element<I, A> e)
 {
-	fms::grassmann::element<N, A> _e;
-
-	for (const auto& [i, a] : e) {
-		_e[~i] = a;
-	}
-
-	return _e;
+	return ~e;
 }
-*/
+
